2241a505e3742b6976eb6fd75ee26cb466bc15f3
tdj03001 <tyjenki@gmail.com>
2020-02-28 22:25:57 -0500

    created burger model

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,26 @@
 const orm = require("../config/orm");
 
+const burger = {
+  all: function (callback) {
+    orm.all("burgers", function (res) {
+      callback(res);
+    });
+  },
+  create: function (cols, vals, callback) {
+    orm.create("burgers", cols, vals, function (res) {
+      callback(res);
+    });
+  },
+  update: function (objColVals, condition, callback) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      callback(res);
+    });
+  },
+  delete: function (condition, callback) {
+    orm.update("burgers", condition, function (res) {
+      callback(res);
+    });
+  }
+};
 
-
-
-
-
-
-module.exports = x;
+module.exports = burger;
