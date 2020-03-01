d7438e56dffdba81845088b1acffc0ea29300dcf
tdj03001 <tyjenki@gmail.com>
2020-02-29 19:25:41 -0500

    added express.static

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_controller");
-
+const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function () {
