1cd46652330215f61bb023d2aeb8d74486d3392b
tdj03001 <tyjenki@gmail.com>
2020-02-29 21:31:07 -0500

    updated seed names

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,8 @@ INSERT INTO burgers
   (burger_name, devoured)
 VALUES
   ("Holy Aioli", false),
-  ("Iron works bbq", false),
+  ("Sneaky Meat", false),
   ("Da Bomb Burgah", false),
   ("Old 96'er", false),
-  ("Hambuglar", false);
+  ("Hambuglar", false),
+  ("Scarf it like it's hot", false);
