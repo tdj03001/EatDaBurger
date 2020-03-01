241870f3c0dc1f375b257c00fd8c6e7250516576
tdj03001 <tyjenki@gmail.com>
2020-02-29 19:19:39 -0500

    cleaned up formatting

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ const connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-// Make connection.
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +16,4 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
