41be67edea1edff1aab2795fe9d2a85089adb16a
tdj03001 <tyjenki@gmail.com>
2020-02-29 19:03:43 -0500

    new seeded burger names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
 INSERT INTO burgers
   (burger_name, devoured)
 VALUES
+  ("Holy Aioli", false),
   ("Iron works bbq", false),
   ("Da Bomb Burgah", false),
-  ("Old 96'er", false);
+  ("Old 96'er", false),
+  ("Hambuglar", false);
