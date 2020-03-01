89a61560ad9aae473ce3f3de96824a97b257c7fd
tdj03001 <tyjenki@gmail.com>
2020-02-29 19:21:37 -0500

    fixed issue where router.post was looking for 'name' rather than 'burger_name'

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,46 +3,32 @@ const router = express.Router();
 const burger = require("../models/burger");
 
 router.get("/", function (req, res) {
-  burger.all(function (data) {
-    const hbsObj = {
+  burger.selectAll(function (data) {
+    const hbsObject = {
       burgers: data
     };
-    console.log(hbsObj);
-    res.render("index", hbsObj);
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
 router.post("/api/burgers", function (req, res) {
-  burger.create([
-    "name", "devoured"
+  burger.insertOne([
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name, req.body.devoured
   ], function (result) {
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burgers/:id", function (req, res) {
-  const status = "id = " + req.params.id;
-
-  console.log("status", status);
-
-  burger.update({
+  const condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.updateOne({
     devoured: req.body.devoured
-  }, status, function (result) {
-    if (result.changedRows === 0) {
-      return res.statusMessage(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
-  const status = "id = " + req.params.id;
-
-  burger.delete(status, function (result) {
-    if (result.affectedRows === 0) {
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -50,5 +36,16 @@ router.delete("/api/burgers/:id", function (req, res) {
   });
 });
 
+// router.delete("/api/burgers/:id", function (req, res) {
+//   const condition = "id = " + req.params.id;
+
+//   burger.delete(condition, function (result) {
+//     if (result.affectedRows == 0) {
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 module.exports = router;
