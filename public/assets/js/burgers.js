9dc8c1555687e01444330044f69a7c87502086ba
tdj03001 <tyjenki@gmail.com>
2020-02-29 19:47:06 -0500

    removed console.log

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -29,7 +29,7 @@ $(function () {
       data: newBurger
     }).then(
       function () {
-        console.log("created new cat");
+        console.log("created new burger");
         location.reload();
       }
     );
