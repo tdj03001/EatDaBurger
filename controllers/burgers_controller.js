db40dba731b82a2fa2551332103f34a0127be25f
tdj03001 <tyjenki@gmail.com>
2020-02-29 19:42:46 -0500

    removed developement comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -36,16 +36,4 @@ router.put("/api/burgers/:id", function (req, res) {
   });
 });
 
-// router.delete("/api/burgers/:id", function (req, res) {
-//   const condition = "id = " + req.params.id;
-
-//   burger.delete(condition, function (result) {
-//     if (result.affectedRows == 0) {
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
 module.exports = router;
